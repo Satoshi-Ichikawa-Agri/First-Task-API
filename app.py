c23ea3f36803d3864949e2fa7182be220ea7434b
Satoshi-Ichikawa-Agri <stayhungry.stayfoolish.satoshi@gmail.com>
2022-11-08 20:22:51 +0900

    test app.py

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
 """ 要件
 1. 猫、犬、狐の画像が表示される以下のAPIを利用して、以下のgifのように選択した動物の画像が表示されるようにしてください。
 """
+import io
+import json
+import requests
 from flask import Flask
 from flask import render_template
-from flask import request
+from PIL import Image
 
 
 # Flaskオブジェクトを作成
 app = Flask(__name__)
 
 
+# GET Index
 @app.route("/")
-def Index():
+def index():
     return render_template("Index.html")
 
 
-@app.route("/selectAnimal", methods=["POST"])
-def selectAnimal():
+@app.route("/Animal", methods=["POST"])
+def select_animal():
+    return render_template("Animal.html")
+
+
+def image():
+    url = "https://dog.ceo/api/breeds/image/random"
+
+    # (1)APIを実行
+    responce = requests.get(url)
+
+    # (2) 返ってきたJSONを処理
+    jsonObj = responce.json()
+    ImageUrl = jsonObj["message"]
+    strImageURL = str(ImageUrl)
+
+    # (3) 画像URLから画像表示
+    file = io.BytesIO(requests.get(strImageURL).content)
     img = Image.open(file)
-    img.save("images/test.png")
+    img.save("images/out.png")
     img.show()
 
 
