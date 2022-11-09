74f205e11b05b50aae3e76c655de617040c320ab
Satoshi-Ichikawa-Agri <stayhungry.stayfoolish.satoshi@gmail.com>
2022-11-10 05:58:59 +0900

    modefy app.py

--- app.py
+++ app.py
@@ -1,47 +1,35 @@
 """ 要件
 1. 猫、犬、狐の画像が表示される以下のAPIを利用して、以下のgifのように選択した動物の画像が表示されるようにしてください。
 """
-import io
-import json
 import requests
 from flask import Flask
 from flask import render_template
 from PIL import Image
 
 
-# Flaskオブジェクトを作成
-app = Flask(__name__)
+app = Flask(__name__, static_folder="FirstWeekTasks\First-Task-API\images\dog.png")
 
 
-# GET Index
 @app.route("/")
 def index():
     return render_template("Index.html")
 
 
-@app.route("/Animal", methods=["POST"])
-def select_animal():
-    return render_template("Animal.html")
-
-
-def image():
+def main():
     url = "https://dog.ceo/api/breeds/image/random"
-
-    # (1)APIを実行
-    responce = requests.get(url)
-
-    # (2) 返ってきたJSONを処理
-    jsonObj = responce.json()
-    ImageUrl = jsonObj["message"]
-    strImageURL = str(ImageUrl)
-
-    # (3) 画像URLから画像表示
-    file = io.BytesIO(requests.get(strImageURL).content)
-    img = Image.open(file)
-    img.save("images/out.png")
-    img.show()
+    request = requests.get(url)
+    # print(request) → Response[200]
+    image_data = request.json()
+    image_url = image_data["message"]
+    download_image(url=image_url, file_path="images/dog.png")
 
 
-# Conduct application
+def download_image(url, file_path):
+    request = requests.get(url, stream=True)
+    if request.status_code == 200:
+        with open(file_path, "wb") as f:
+            f.write(request.content)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
