a98a10dc97722d5f8bfeb8678e855f8bd98aaa36
Satoshi-Ichikawa-Agri <stayhungry.stayfoolish.satoshi@gmail.com>
2022-11-11 20:33:39 +0900

    modefy app.py

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
 """ 要件
 1. 猫、犬、狐の画像が表示される以下のAPIを利用して、以下のgifのように選択した動物の画像が表示されるようにしてください。
 """
+import json
 import requests
+import secrets
 from flask import Flask
 from flask import render_template
-from PIL import Image
+from flask import request
 
 
-app = Flask(__name__, static_folder="FirstWeekTasks\First-Task-API\images\dog.png")
+# Flaskオブジェクトの生成
+app = Flask(__name__)
 
 
+# セッション情報を暗号化するためのキー設定
+app.secret_key = secrets.token_urlsafe(32)
+
+
+# GET index
 @app.route("/")
 def index():
-    return render_template("Index.html")
+    return render_template("index.html")
 
 
-def main():
-    url = "https://dog.ceo/api/breeds/image/random"
-    request = requests.get(url)
-    # print(request) → Response[200]
-    image_data = request.json()
-    image_url = image_data["message"]
-    download_image(url=image_url, file_path="images/dog.png")
-
-
-def download_image(url, file_path):
-    request = requests.get(url, stream=True)
-    if request.status_code == 200:
-        with open(file_path, "wb") as f:
-            f.write(request.content)
+# POST Animal
+@app.route("/animal", methods=["POST"])
+def animal():
+    if request.form["selectAnimal"] == "cat":
+        res = requests.get("https://api.thecatapi.com/v1/images/search")
+        outfile = json.loads(res.text)[0]["url"]
+    elif request.form["selectAnimal"] == "dog":
+        res = requests.get("https://dog.ceo/api/breeds/image/random")
+        outfile = json.loads(res.text)["message"]
+    else:
+        res = requests.get("https://randomfox.ca/floof")
+        outfile = json.loads(res.text)["image"]
+    return render_template("animal.html", outfile=outfile)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
